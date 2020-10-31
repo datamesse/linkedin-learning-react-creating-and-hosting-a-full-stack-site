f0575fa2377ca64d8ae3571a2eb37d9399ac7355
마쿠하리 <data_messe@hotmail.com>
2020-10-31 14:38:37 +1100

    First commit

---
+++ src/build/precache-manifest.65a5b9f0a0566b5a943b6ca7b59e3583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f11b832d2bff2ae667d84d35f1ad44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42ba077c7bef952ede7",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42ba077c7bef952ede7",
+    "url": "/static/js/main.73c1bc98.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
